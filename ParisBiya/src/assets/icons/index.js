60b9b8af68c25d1435c2fd8403d951b726cafb2f
AndrewBiya <90545528+AndrewBiya@users.noreply.github.com>
2023-05-10 00:40:36 +0800

    Delete ParisBiya directory

--- ParisBiya/src/assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-import Logo from './Logo.svg';
-import IconBack from './icon-back.svg';
-import IconHome from './icon-home.svg';
-import IconReport from './icon-report.svg';
-
-export {Logo, IconBack, IconHome, IconReport};
--- ParisBiya/src/screens/index.js
+++
@@ -1,9 +0,0 @@
-import SplashScreen from './SplashScreen';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import Home from './Home';
-import Report from './Report';
-import CashOnHand from './CashOnHand';
-import CashOnBank from './CashOnBank';
-
-export {SplashScreen, SignIn, SignUp, Home, Report, CashOnHand, CashOnBank};
