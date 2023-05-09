a21f5435513ccc7308930d7f4bd59bcf89a8e662
AndrewBiya <90545528+AndrewBiya@users.noreply.github.com>
2023-05-09 23:59:40 +0800

    Delete ParisBiya directory

--- ParisBiya/src/components/molecules/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header';
-import TextInput from './TextInput';
-import HomeHeader from './HomeHeader';
-import Footer from './Footer';
-import Card from './Card';
-
-export {Header, TextInput, HomeHeader, Footer, Card};
